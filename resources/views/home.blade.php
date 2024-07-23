97112e0c0e86ac4a2ec74855ed3c3824a3c9333f
farrasrifqi <farrasrifqi25@gmail.com>
2024-07-23 15:10:55 +0700

    Menambahkan navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,8 +12,34 @@
 </head>
 
 <body>
-    <h1>Ini adalah halamn home</h1>
-    <h2>Selamat datang, {{ $name }} Anda adalah {{ $role }}</h2>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Blade Templating</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Product</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container mt-5">
+        <h1>Ini adalah halamn home</h1>
+        <h2>Selamat datang, {{ $name }} Anda adalah {{ $role }}</h2>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
