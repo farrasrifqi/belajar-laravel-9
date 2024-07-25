2e7aecac04ff1075e772849b6ed26a72802ea5ff
farrasrifqi <farrasrifqi25@gmail.com>
2024-07-25 21:18:53 +0700

    get data menggunakan query builder

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Student;
 use Doctrine\DBAL\Types\Type;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class StudentController extends Controller
 {
     public function index()
     {
-        $student = Student::all();
-        return view('student', ['studentList' => $student]);
+        // $student = Student::all();
+        // return view('student', ['studentList' => $student]);
+
+        //query builder
+        $student = DB::table('students')->get();
+        dd($student);
+        
     }
 }
