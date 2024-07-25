4973a2007ff68e67af74633b5f8dfad0e517410a
farrasrifqi <farrasrifqi25@gmail.com>
2024-07-25 15:01:57 +0700

    Mengisis factory menggunakan faker

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Sutdent>
@@ -16,8 +17,12 @@ class StudentFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = Faker\Factory::create();
         return [
-            
+            'name' => $faker->name(),
+            'gender' => Arr::random(['L'. 'P']),
+            'nis' => mt_rand(0000001, 9999999),
+            'class_id' => Arr::random([1, 2, 3, 4]),
         ];
     }
 }
