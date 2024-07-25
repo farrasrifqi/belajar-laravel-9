03508cc45446b34c0549f434c7ec3bc029144d35
farrasrifqi <farrasrifqi25@gmail.com>
2024-07-25 22:09:04 +0700

    update data menggunakan query builder

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -16,23 +16,30 @@ class StudentController extends Controller
 
         //query builder
         // $student = DB::table('students')->get();
+        
         //create
-        DB::table('students')->insert([
-            'name' => 'query builder',
-            'gender' => 'L',
-            'nis' => '0167289',
-            'class_id' => 1
+        // DB::table('students')->insert([
+        //     'name' => 'query builder',
+        //     'gender' => 'L',
+        //     'nis' => '0167289',
+        //     'class_id' => 1
+        // ]);
+
+        //update
+        DB::table('students')->where('id', 26)->update([
+            'name' => 'query builder 2',
+            'class_id' => 3
         ]);
 
         // eloquent
         // $student = Student::all();
         //create
-        Student::create([
-            'name' => 'eloquent',
-            'gender' => 'p',
-            'nis' => '0163419',
-            'class_id' => 2
-        ]);
+        // Student::create([
+        //     'name' => 'eloquent',
+        //     'gender' => 'p',
+        //     'nis' => '0163419',
+        //     'class_id' => 2
+        // ]);
         
         // dd($student);
 
