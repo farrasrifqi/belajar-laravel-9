117efe5bf3648d5282d8c9895f686d8e7a43594f
farrasrifqi <farrasrifqi25@gmail.com>
2024-07-25 13:12:16 +0700

    SEEDER=Truncate table pada seeder

--- database/seeders/ClassSeeder.php
+++ database/seeders/ClassSeeder.php
@@ -6,14 +6,19 @@ use App\Models\ClassRoom;
 use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class ClassSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
+        Schema::disableForeignKeyConstraints();
+        ClassRoom::truncate();
+        Schema::enableForeignKeyConstraints();
+        
         ClassRoom::insert([
             'name' => 'Aiu',
             'created_at' => Carbon::now(),
