9117a4e6f4c1db8e48640b05ec5cd38224579cd7
farrasrifqi <farrasrifqi25@gmail.com>
2024-07-26 15:51:55 +0700

    cara kerja lazy loud & eager loud

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -9,7 +9,17 @@ class ClassController extends Controller
 {
     public function index()
     {
+        // lazy loud
         $class = ClassRoom::all();
+        // select * from table class
+        // select * from student where class = 1A
+        // select * from student where class = 1B
+        // select * from student where class = 1C
+        // select * from student where class = 1D
+
+        // eager loud
+        // select * from table class
+        // select * from student where class in (1a,1b, 1c, 1d)
         return view('classroom', ['classList' => $class]);
     }
 }
